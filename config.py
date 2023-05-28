8bef45e8a8aa622b75d2e3d7a3702f1bc0f6c817
hsh819 <61219683+hsh819@users.noreply.github.com>
2023-05-28 09:43:09 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # [step 1]>> 例如： API_KEY = "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" （此key无效）
 # import os
 # API_KEY = os.environ("OPENAI_API_KEY")
-# API_KEY = "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"    # 可同时填写多个API-KEY，用英文逗号分割，例如API_KEY = "sk-openaikey1,sk-openaikey2,fkxxxx-api2dkey1,fkxxxx-api2dkey2"
+API_KEY = "sk-此处填API密钥"    # 可同时填写多个API-KEY，用英文逗号分割，例如API_KEY = "sk-openaikey1,sk-openaikey2,fkxxxx-api2dkey1,fkxxxx-api2dkey2"
 
 # [step 2]>> 改为True应用代理，如果直接在海外服务器部署，此处不修改
 USE_PROXY = False
@@ -41,7 +41,7 @@ DARK_MODE = True  # "LEFT-RIGHT"（左右布局） # "TOP-DOWN"（上下布局�
 TIMEOUT_SECONDS = 30
 
 # 网页的端口, -1代表随机端口
-WEB_PORT = 9000
+WEB_PORT = -1
 
 # 如果OpenAI不响应（网络卡顿、代理失败、KEY失效），重试的次数限制
 MAX_RETRY = 2
